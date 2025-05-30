517defe15d033d4848100d314f1fc2be4885a5f5
Chege Wakiama <117751299+munyanyaguo@users.noreply.github.com>
2025-05-30 15:10:21 +0300

    Update App.jsx
    Fetched and added addExpense feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './header';
 import ExpenseForm from './expenseForm';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [expense, setExpense] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [showForm, setShowForm] = useState(false);
+  const [importantExpenses, setImportantExpenses] = useState({});
+  const [editingExpense, setEditingExpense] = useState(null);
+  const [showImportantOnly, setShowImportantOnly] = useState(false);
 
-  const handleAddExpense = (expense) => {
-    console.log('Expense added:', expense);
+
+  const API_BASE_URL = "https://group-13-project.onrender.com/"
+
+  useEffect(() => {
+    fetchExpenses();
+  }, []);
+
+  const fetchExpenses = async () => {
+    setLoading(true);
+    try {
+      const response await fetch(`${API_BASE_URL}expenses`);
+      if (response.ok) {
+        const data = await response.json();
+        setImportantExpenses(data);
+      } else {
+        console.error("Failed to fetch expenses:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching expenses:", error);
+    } finally {
+      setLoading(false);
+    }
   };
+
 
   return (
     <>
