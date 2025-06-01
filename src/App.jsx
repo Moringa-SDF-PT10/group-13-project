c8ec7a1f5fcc4276d2fef5582dd5ad918bcddaa3
Chege Wakiama <117751299+munyanyaguo@users.noreply.github.com>
2025-06-01 12:56:20 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './header';
 import ExpenseForm from './expenseForm';
-import viteLogo from '/vite.svg';
-import reactLogo from './assets/react.svg';
-
 
 function App() {
   const [expenses, setExpenses] = useState([]);
@@ -66,7 +63,6 @@ function App() {
     }
   };
 
-
    const updateExpense = async (updatedExpense) => {
     setLoading(true);
     try {
@@ -98,32 +94,7 @@ function App() {
     }
   };
 
-  return (
-    <>
-      <Header />
-      <ExpenseForm onAddExpense={addExpense} />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  
 }
 
 export default App
