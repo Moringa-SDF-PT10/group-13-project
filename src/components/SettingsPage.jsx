40e42a9835a326de4bba24be064f2fe81e8cb042
Chege Wakiama <117751299+munyanyaguo@users.noreply.github.com>
2025-06-03 00:27:28 +0300

    Update SettingsPage.jsx

--- src/components/SettingsPage.jsx
+++ src/components/SettingsPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './Header';
-import '../styles/SettingsPage.css';
+
 const SettingsPage = () => {
     return (
         <div className="settings-page">
